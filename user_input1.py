45b5dc576e197ae76c578f866472daee4fa7760f
Gloria Khamaya <gloriamaleche@gmail.com>
2024-02-25 20:50:21 +0300

    Python user input

---
+++ user_input1.py
@@ -0,0 +1,5 @@
+name=input('Hello! I would like to get to know you. What is your name? ')
+age=input('How about your age? ')
+location=input('And can I meet you? Where can I find you? ')
+
+print('Great! Let us start over. Hello, '+name+'. You are '+age+' years old and you live in '+location+ '.') 
